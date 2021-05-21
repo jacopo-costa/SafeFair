0891288462911cdb67059663dda354bcb6530011
Gekobix <49352077+Gekobix@users.noreply.github.com>
2021-05-21 15:19:23 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,15 +1,20 @@
-//script che permette al menù di seguire l'utente nella pagina
-window.onscroll = function() {myFunction()};
-            
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-            
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 60) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0;
 }
 
 //script per lo slideshow, cambio manuale
