cc9f5dad58e2ac4c6a15c5ac96f9dd23b8a2f442
Gekobix <costa.jacopo@gmail.com>
2021-09-14 16:14:55 +0200

    Back to square 1

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sqlite3 = require('sqlite3');
+// imports
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const sqlite3 = require('sqlite3');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(session({
+	secret: 'mysecret',
+	resave: false,
+	saveUninitialized: false
+  }))
 
 // Connessione al DB
 let db = new sqlite3.Database('safefair.db', (err) => {
@@ -26,26 +25,52 @@ let db = new sqlite3.Database('safefair.db', (err) => {
 	  console.error(err.message);
 	}
 	console.log('Connesso al DB SafeFair');
-  });
+  })
 
-app.use('/', indexRouter);
-app.use('/search', searchRouter);
-app.use('/:id', fieraRouter);
+// porta
+const port = 3000
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Ascolta sulla porta 3000
+app.listen(port, () => console.info(`Ascolto su porta ${port}`))
+
+// GETs
+app.get('/', (req,res) => {
+  	db.all("SELECT * FROM Fiere ORDER BY id limit 5", [], (err, rows) => {
+    	if (err) {
+    	  return console.error(err.message)
+   		}
+		db.all("SELECT news FROM News ORDER BY timestamp DESC", [], (err, righe) => {
+			if (err) {
+				return console.error(err.message)
+			}
+			res.render("index", {fiere: rows, news: righe})
+	})
+  })
+})
+
+app.get('/:id', (req, res) => {
+	db.get("SELECT * FROM Fiere WHERE id = " + req.params.id, (err, row) => {
+		if (err) {
+			return console.error(err.message)
+		  }
+		  res.render("fiera", { fiera: row })
+	})
+})
+
+app.get('/search', (req, res) => {
+
+	var x = req.query.q;
+	db.all(`SELECT * FROM Fiere WHERE (nome LIKE '%${x}%' OR descrizione LIKE '%${x}%' OR tag LIKE '%${x}%')`, (err, rows) => {
+		if (err) {
+			return console.error(err.message)
+		  }
+		  res.render("search", { x, fiere: rows })
+	})
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+//db.close((err) => {
+//	if (err) {
+//	  console.error(err.message);
+//	}
+//	console.log('Close the database connection.');
+  //});
