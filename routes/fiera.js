1aa425b1786933b1ce44b93933b48f2dec88b9d1
Gekobix <costa.jacopo@gmail.com>
2021-09-16 22:49:03 +0200

    fe

--- routes/fiera.js
+++ routes/fiera.js
@@ -2,6 +2,15 @@ const express = require("express");
 const db = require("../db");
 const router = express.Router();
 
+router.get("/all", (req, res) => {
+  db.all("SELECT * FROM Fiere", [], (err, rows) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("fiere", { fiere: rows });
+  });
+});
+
 router.get("/:id", (req, res) => {
   db.get("SELECT * FROM Fiere WHERE id = " + req.params.id, (err, row) => {
     if (err) {
